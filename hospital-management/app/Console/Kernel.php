3fa85742b8de6100f6f02c242fb0ddd923e88821
farrelaranku <flinkin16@gmail.com>
2023-12-05 00:14:15 +0700

    add cron job

--- hospital-management/app/Console/Kernel.php
+++ hospital-management/app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\DeleteExpiredNews;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +13,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        // setiap 10 detik
+        $schedule->job(new DeleteExpiredNews)->everyTenSeconds();
+        
     }
 
     /**
